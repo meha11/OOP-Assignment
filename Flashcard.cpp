c179def10e668d40e1b57da07fdf86cd8e4eb59c
Meharaj Ganasen <11meharaj@gmail.com>
2025-06-13 20:47:57 +0800

    Initialize libarary and First class

---
+++ Flashcard.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+class FlashCard {
+protected:
+    string question;
+    string answer;
+    int score;
