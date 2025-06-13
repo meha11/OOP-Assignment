cb2081e3980c85293c691b86a35b294e5972c26f
Meharaj Ganasen <11meharaj@gmail.com>
2025-06-13 20:54:54 +0800

    Completed Class Flashcard

--- Flashcard.cpp
+++ Flashcard.cpp
@@ -8,6 +8,45 @@ using namespace std;
 
 class FlashCard {
 protected:
-    string question;
-    string answer;
+    string quest;
+    string ans;
     int score;
+
+public:
+    FlashCard(string q, string a, int s = 0) : quest(q), ans(a), score(s) {}
+
+    void showCard()
+    {
+        cout << "Q: " << quest << endl;
+    }
+
+    bool checkAnswer(const string& userAnswer)
+    {
+        return userAnswer == ans;
+    }
+
+    void adjustScore(bool correct)
+    {
+        score += (correct ? 1 : -1);
+        if (score < 0) score = 0;
+    }
+
+    string getQuestion() const { return quest; }
+    string getAnswer() const { return ans; }
+    int getScore() const { return score; }
+
+    void saveToFile(ofstream& out) const
+    {
+        out << quest << "|" << ans << "|" << score << endl;
+    }
+
+    static FlashCard loadFromLine(const string& line)
+    {
+        size_t p1 = line.find('|');
+        size_t p2 = line.find('|', p1 + 1);
+        string q = line.substr(0, p1);
+        string a = line.substr(p1 + 1, p2 - p1 - 1);
+        int s = stoi(line.substr(p2 + 1));
+        return FlashCard(q, a, s);
+    }
+};
